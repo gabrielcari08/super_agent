7b2a0bdb658c0010bd8d4df070546d1e118e32c8
gabrielcari08 <gabrielcari2008@gmail.com>
2025-08-10 14:56:55 -0300

    tablas creadas de forma correcta luego de corregir unos problemas

--- app/core/config.py
+++ app/core/config.py
@@ -9,10 +9,6 @@ class Settings(BaseSettings):
     postgres_host: str = "localhost"
     postgres_port: str = "5432"
     
-    secret_key: str
-    algorithm: str = "HS256"
-    
-
     class Config:
         env_file = ".env"
 
